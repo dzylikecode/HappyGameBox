3c6bc89db12746220795e0a57eadb07b9f9bd204
DZ <1494977853@qq.com>
2021-10-13 23:21:33 +0800

    feature: add string format output

--- Windows/Win32/Win32_Interface/La_StringClass.h
+++ Windows/Win32/Win32_Interface/La_StringClass.h
@@ -91,6 +91,18 @@ inline ASTRING& AToA(__in LPCCH pszInBuf) { static ASTRING temp; return temp = p
 #endif
 
 
+template< typename... Args >
+std::string string_sprintf(const char* format, Args... args) {
+	int length = std::snprintf(nullptr, 0, format, args...);
+	assert(length >= 0);
+
+	char* buf = new char[length + 1];
+	std::snprintf(buf, length + 1, format, args...);
+
+	std::string str(buf);
+	delete[] buf;
+	return std::move(str);
+}
 
 
 
